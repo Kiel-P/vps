74fb40e954ebf37adab1f9ef27752af88e2d10bb
KIHEO <kielhjs6@gmail.com>
2026-01-10 19:30:35 +0700

    Create systemd.sh

---
+++ core/systemd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "[INFO] Setup systemd xray"
+
+cat > /etc/systemd/system/xray.service <<EOF
+[Unit]
+Description=Xray Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
+Restart=on-failure
+LimitNOFILE=1000000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable xray
