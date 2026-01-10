6d240071e852b530c6e10efac3e919bf4f057089
KIHEO <kielhjs6@gmail.com>
2026-01-10 19:29:35 +0700

    Create domain.sh

---
+++ core/domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DOMAIN_FILE="/etc/zivpn/domain"
+mkdir -p /etc/zivpn
+
+if [[ ! -f "$DOMAIN_FILE" ]]; then
+  read -rp "Masukkan domain VPS: " DOMAIN
+  echo "$DOMAIN" > "$DOMAIN_FILE"
+else
+  DOMAIN=$(cat "$DOMAIN_FILE")
+fi
+
+export DOMAIN
+echo "[INFO] Domain digunakan: $DOMAIN"
