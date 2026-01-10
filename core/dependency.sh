662c463e92d9264664d43023a0b7ff55b86a25a8
KIHEO <kielhjs6@gmail.com>
2026-01-10 20:01:04 +0700

    Update dependency.sh

--- core/dependency.sh
+++ core/dependency.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "[INFO] Install dependency..."
+echo "[INFO] Install dependency"
+
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt install -y curl wget unzip socat cron certbot
